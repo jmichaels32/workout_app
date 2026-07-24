c993537ad50708fd47460a7d574e5ac168d9c9ee
Jack Michaels <jackfm@stanford.edu>
2026-07-24 10:26:39 -0700

    Refine mobile calendar and movement detail

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "movement-library-mvp-v87";
+const CACHE_NAME = "movement-library-mvp-v88";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon.svg", "data/movements.json"];
 
 self.addEventListener("install", event => {
