da3384213b1ec00811fff178422da9b506bef650
Jack Michaels <jackfm@stanford.edu>
2026-07-24 10:06:08 -0700

    Tighten mobile detail layouts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "movement-library-mvp-v86";
+const CACHE_NAME = "movement-library-mvp-v87";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon.svg", "data/movements.json"];
 
 self.addEventListener("install", event => {
