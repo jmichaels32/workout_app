8d891c4850bb5caafd87e806e601a99e0d42eb25
Jack Michaels <jackfm@stanford.edu>
2026-07-25 13:39:04 -0700

    Fix hip thrust anatomy maps

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "movement-library-mvp-v104";
+const CACHE_NAME = "movement-library-mvp-v105";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon.svg", "assets/icons/sort-gear.png", "data/movements.json"];
 
 self.addEventListener("install", event => {
