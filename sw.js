2c449151a4a7d51d05b0d88550dd539f616e7f60
Jack Michaels <jackfm@stanford.edu>
2026-07-25 12:54:43 -0700

    Separate sort direction panel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "movement-library-mvp-v101";
+const CACHE_NAME = "movement-library-mvp-v102";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon.svg", "assets/icons/sort-gear.png", "data/movements.json"];
 
 self.addEventListener("install", event => {
