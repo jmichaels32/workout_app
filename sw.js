3bf48fd0ec62a5a28c2f4db3b86ccf6601aefd55
Jack Michaels <jackfm@stanford.edu>
2026-07-26 20:28:59 -0700

    Streamline app shell code

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "movement-library-mvp-v111";
+const CACHE_NAME = "movement-library-mvp-v112";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon.svg", "assets/icons/sort-gear.png", "data/movements.json"];
 
 self.addEventListener("install", event => {
