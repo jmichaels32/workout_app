4892517528536d3164c4b6bdeec0aa0064cf98b1
Jack Michaels <jackfm@stanford.edu>
2026-07-30 16:34:15 -0700

    Add biceps curl and pull day movements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "movement-library-mvp-v112";
+const CACHE_NAME = "movement-library-mvp-v113";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon.svg", "assets/icons/sort-gear.png", "data/movements.json"];
 
 self.addEventListener("install", event => {
